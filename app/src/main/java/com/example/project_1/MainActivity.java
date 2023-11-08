01fc7eb808a52f3b6dfd531d36c4fef614da483a
AndrewPyscher <andypyscher@gmail.com>
2023-11-07 20:50:32 -0500

    Add the api calls create a singleton dictionary class

--- app/src/main/java/com/example/project_1/MainActivity.java
+++ app/src/main/java/com/example/project_1/MainActivity.java
@@ -1,32 +1,150 @@
 package com.example.project_1;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnQuiz;
-    Button btnBrowseWords;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class MainActivity extends AppCompatActivity {
+    static String word = "";
+    static int count = 0;
+    ArrayList<Integer> nums;
+    ArrayList<String> listOfWords, selectedWords;
+    static String definition = "", synonym = "";
+
+    ExecutorService executorService;
+    static RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnQuiz = findViewById(R.id.btnQuiz);
-        btnBrowseWords = findViewById(R.id.btnBrowseWords);
+        queue =  Volley.newRequestQueue(this);
+        nums = new ArrayList<>();
+        listOfWords = new ArrayList<>();
+        selectedWords = new ArrayList<>();
 
-        btnQuiz.setOnClickListener(e->{
-            //to be set later when quiz class is created
-            Intent intent = new Intent(this, Quiz.class);
-            startActivity(intent);
+
+        executorService = Executors.newSingleThreadExecutor();
+
+
+         //start thread
+        runOnUiThread(()->{
+            Dictionary.createDictionary();
+            readFile();
+
         });
-        btnBrowseWords.setOnClickListener(e->{
-            //to be set later when browse words activity is created
-            Intent intent = new Intent(this, BrowseWords.class);
-            startActivity(intent);
-        });
+
     }
+
+    public void readFile(){
+        InputStream is = getResources().openRawResource(R.raw.words);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        try {
+            String line = br.readLine();
+            while(line!=null){
+                // only add words with at least 3 characters
+                if(line.length() > 2 ){
+                    listOfWords.add(line);
+                }
+                line = br.readLine();
+
+            }
+            Random r = new Random();
+            int random;
+            while(selectedWords.size() < 100){
+                random = r.nextInt(listOfWords.size() -1);
+                if(!nums.contains(random)){
+                    nums.add(random);
+                    selectedWords.add(listOfWords.get(random));
+                    word = listOfWords.get(random);
+                    getDef();
+                }
+            }
+
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static void getDef(){
+            try {
+                String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+                JsonArrayRequest r = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
+                    try {
+                        JSONObject arr = response.getJSONObject(0);
+                        JSONArray meaning = arr.getJSONArray("meanings");
+                        JSONObject first = meaning.getJSONObject(0);
+                        JSONArray definitions = first.getJSONArray("definitions");
+                        JSONObject def = definitions.getJSONObject(0);
+                        definition = def.getString("definition");
+                        //def.getJSONArray("definition");
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }, error -> {
+                    Log.d("error", "error: " + Arrays.toString(error.getStackTrace()));
+                });
+
+                queue.add(r);
+
+            }catch (Exception e){
+                definition = "null";
+            }
+    }
+
+    public static void getSyn(){
+        try {
+            String url = "https://api.api-ninjas.com/v1/thesaurus?word="+ word + "&X-Api-Key=hIHbhVmlkuSbUvkEnuvyhg==YAsEJ2nPEgRNcOpk";
+            JsonObjectRequest r = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+                try {
+                    String word = response.getString("word");
+                    JSONArray synonyms = response.getJSONArray("synonyms");
+                    ArrayList<String> temp = new ArrayList<>();
+                    temp.add(synonyms.getString(0));
+                    temp.add(synonyms.getString(1));
+                    temp.add(synonyms.getString(2));
+
+                    Dictionary.dictionary.add(new DictionaryItem(word, definition, temp, false));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }, error -> {
+                Log.d("error", "error: " + Arrays.toString(error.getStackTrace()));
+            });
+
+            queue.add(r);
+
+        }catch (Exception e){
+            definition = "null";
+        }
+    }
+
+
+
 }

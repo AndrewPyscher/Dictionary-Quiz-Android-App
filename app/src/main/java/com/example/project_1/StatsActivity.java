d1fb895cb1bc5141d47bc1987d8a506d83d51e8f
Caleb Hunter <64340885+calebhunt5@users.noreply.github.com>
2023-11-09 16:28:13 -0500

    Added functionality for time, grade, and navigation buttons to Stats activity

--- app/src/main/java/com/example/project_1/StatsActivity.java
+++ app/src/main/java/com/example/project_1/StatsActivity.java
@@ -2,6 +2,8 @@ package com.example.project_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,6 +28,86 @@ public class StatsActivity extends AppCompatActivity {
 
         tvTime = findViewById(R.id.tvTime);
         tvPercentageCorrect = findViewById(R.id.tvCorrectAnswersPercent);
+        btnBackToMain = findViewById(R.id.btnBackToMain);
+        btnPlayAgain = findViewById(R.id.btnPlayAgain);
+
+        //get intent from quiz that holds millisecondsToFinish, the number of correctly answered questions, and the total number of questions
+        Intent quizIntent = getIntent();
+        millisecondsToFinish = quizIntent.getIntExtra("totalTime", -1);
+        correctAnswers = quizIntent.getIntExtra("correctAnswers", -1);
+        numOfQuestions = quizIntent.getIntExtra("numOfQuestions", -1);
+
+
+        updateTime(millisecondsToFinish);
+        updateTimeColor(millisecondsToFinish);
+
+        //five out of 10 as a place holder until quiz activity is working
+        updatePercentageCorrect(5, 10);
+        updatePercentageCorrectColor(5, 10);
+
+        btnPlayAgain.setOnClickListener(e->{
+            Intent playAgainIntent = new Intent(this, Quiz.class);
+            startActivity(playAgainIntent);
+        });
+
+        btnBackToMain.setOnClickListener(e->{
+            Intent backToMainIntent = new Intent(this, MainActivity.class);
+            startActivity(backToMainIntent);
+        });
 
     }
+
+    //make time background color green if under 30 seconds, yellow if under 1 minute, and red if over 1 minute
+    public void updateTimeColor (int milliseconds) {
+        if (milliseconds < 30000) {
+            tvTime.setBackgroundColor(Color.GREEN);
+        }
+        else if (30000 < milliseconds && milliseconds <= 60000) {
+            tvTime.setBackgroundColor(Color.YELLOW);
+        }
+        else if (60000 < milliseconds) {
+            tvTime.setBackgroundColor(Color.RED);
+        }
+    }
+
+    //make percentage correct text color green if above 80%, yellow if above 60%, and red if bellow 60%
+    public void updatePercentageCorrectColor(double correct, double total) {
+        double rawGrade = (correct / total) * 100;
+
+        if (rawGrade >= 80) {
+            tvPercentageCorrect.setBackgroundColor(Color.GREEN);
+        }
+        else if (60 <= rawGrade && rawGrade < 80) {
+            tvPercentageCorrect.setBackgroundColor(Color.YELLOW);
+        }
+        else if (rawGrade < 60) {
+            tvPercentageCorrect.setBackgroundColor(Color.RED);
+        }
+    }
+
+    //display time on tvTime
+    public void updateTime (int milliseconds) {
+        String time = "00:00";
+
+        int totalSeconds = milliseconds / 1000;
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        if (seconds < 10) {
+            time = minutes + ":0" + seconds;
+        }
+        else {
+            time = minutes + ":" + seconds;
+        }
+
+        tvTime.setText(time);
+    }
+
+    //display percentage of correct answers
+    public void updatePercentageCorrect(double correct, double total) {
+        double rawGrade = (correct / total) * 100;
+
+        tvPercentageCorrect.setText(rawGrade + "%");
+    }
+
 }

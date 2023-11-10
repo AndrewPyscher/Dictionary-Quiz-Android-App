47caa16f4304690af15c7906190d6c7cb258a775
ejthelen <ejthelen@svsu.edu>
2023-11-09 23:36:30 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/project_1/MainActivity.java
+++ app/src/main/java/com/example/project_1/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
